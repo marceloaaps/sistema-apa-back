38d198daf575dc17544427f6fe9bef4cc94e9187
Marcelo Alexandre Alves <114123437+marceloaaps@users.noreply.github.com>
2025-04-28 12:07:21 -0300

    Update AnimalController.java

--- src/main/java/com/apa/back/presentation/controllers/AnimalController.java
+++ src/main/java/com/apa/back/presentation/controllers/AnimalController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/animais")
+@RequestMapping("/animals")
 public class AnimalController {
 
     private final AnimalUseCase animalUseCase;
