56dbe7ddb76cb5a27b5a335ffee50e5857c26bf8
Dougcm <dou.costa86@gmail.com>
2025-04-27 23:32:56 -0300

    Implementação de CRUD com soft delete para Animais

---
+++ src/main/java/com/apa/back/core/domain/repositories/AnimalRepository.java
@@ -0,0 +1,22 @@
+package com.apa.back.core.domain.repositories;
+
+import com.apa.back.core.domain.entities.Animal;
+import com.apa.back.presentation.dtos.AnimalDTO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+
+    @Query("SELECT new com.apa.back.presentation.dtos.AnimalDTO(a.id, a.nome, a.raca, a.idade, a.disponivelParaAdocao) FROM Animal a")
+    List<AnimalDTO> findAllAnimalsForAdoption();
+
+    @Query("SELECT new com.apa.back.presentation.dtos.AnimalDTO(a.id, a.nome, a.raca, a.idade, a.disponivelParaAdocao) " +
+            "FROM Animal a WHERE a.id = :id")
+    Optional<AnimalDTO> findAnimalById(Long id);
+}
