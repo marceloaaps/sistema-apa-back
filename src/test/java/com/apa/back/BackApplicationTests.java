c558dcd29014ad564fc95cc6fa1c0f9d82293ddb
Marcelo <marceloalexandreaps@gmail.com>
2025-06-02 02:59:20 -0300

    Add email sending functionality with MailHog support
    Integrated Jakarta Mail for email handling and updated the EmailUseCase to use SMTP. Added MailHog service in docker-compose for local testing and configured application properties for email setup. Modified controller responses to include meaningful messages.

--- src/test/java/com/apa/back/BackApplicationTests.java
+++ src/test/java/com/apa/back/BackApplicationTests.java
@@ -1,13 +1,20 @@
 package com.apa.back;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.main.allow-bean-definition-overriding=true")
+@ActiveProfiles("test")
 class BackApplicationTests {
+
+	@Value("${DB_NAME}")
+	private String dbName;
 
 	@Test
 	void contextLoads() {
+		System.out.println("DB Name: " + dbName);
 	}
 
 }
